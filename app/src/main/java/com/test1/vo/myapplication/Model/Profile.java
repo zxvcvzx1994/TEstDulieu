8c46996615c08f05ca3951dbbb7198965a66bb8c
vinh <12>
2017-11-07 22:13:48 +0700

    Initial commit

---
+++ app/src/main/java/com/test1/vo/myapplication/Model/Profile.java
@@ -0,0 +1,21 @@
+package com.test1.vo.myapplication.Model;
+
+/**
+ * Created by vinh on 11/7/2017.
+ */
+
+public class Profile {
+    private String name;
+
+    public Profile(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
