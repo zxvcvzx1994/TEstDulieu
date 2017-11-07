46bb34e0f386178fe3ec07ee61a770daeedf5512
vinh <12>
2017-11-07 22:16:27 +0700

    update

---
+++ app/src/main/java/com/test1/vo/myapplication/Model/English.java
@@ -0,0 +1,21 @@
+package com.test1.vo.myapplication.Model;
+
+/**
+ * Created by vinh on 11/7/2017.
+ */
+
+public class English {
+    private String subject;
+
+    public English(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
