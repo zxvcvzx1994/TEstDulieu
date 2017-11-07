0dfcf61da3db1c472ce204d4b59a134c962d5e3b
vinh <12>
2017-11-07 22:25:10 +0700

    Changes

---
+++ app/src/main/java/com/test1/vo/myapplication/Model/ThienHao.java
@@ -0,0 +1,21 @@
+package com.test1.vo.myapplication.Model;
+
+/**
+ * Created by vinh on 11/7/2017.
+ */
+
+public class ThienHao {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ThienHao(String name) {
+        this.name = name;
+    }
+}
