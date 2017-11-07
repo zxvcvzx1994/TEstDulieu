1b3e5a8a74f787a5b43420d1551f2ed658c397c3
vinh <12>
2017-11-07 22:19:36 +0700

    Insert

---
+++ app/src/main/java/com/test1/vo/myapplication/Model/Class.java
@@ -0,0 +1,21 @@
+package com.test1.vo.myapplication.Model;
+
+/**
+ * Created by vinh on 11/7/2017.
+ */
+
+public class Class {
+    String Dulieu;
+
+    public Class(String dulieu) {
+        Dulieu = dulieu;
+    }
+
+    public String getDulieu() {
+        return Dulieu;
+    }
+
+    public void setDulieu(String dulieu) {
+        Dulieu = dulieu;
+    }
+}
